7d5fde54e194ef9e61fffe8d43835990ee3e755e
TheSillyP <Petjenorden@gmail.com>
2022-09-22 14:01:34 +0200

    Trying to let both backend and frontend testing work

--- DartTDD/UnitTest1.cs
+++ DartTDD/UnitTest1.cs
@@ -8,12 +8,6 @@ public class UnitTest1
         Assert.Equal(4, Add(2, 2));
     }
 
-    [Fact]
-    public void FailingTest()
-    {
-        Assert.Equal(5, Add(2, 2));
-    }
-
     int Add(int x,int y)
     {
         return x + y;
