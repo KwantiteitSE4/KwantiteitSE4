b08ac3e1d2d7f32de2916f92f8dfe1b358ee08e3
Alexander <alexander.de.haan@student.nhlstenden.com>
2022-09-27 10:41:00 +0200

    Created Database Access Controller

---
+++ KwantiteitSE4/Controllers/DBAccessController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace KwantiteitSE4.Controllers
+{
+    public class DBAccessController
+    {
+        private static DBAccessController instance;
+
+        private DBAccessController(bool reset = false) 
+        {
+            if (reset) Reset();
+        }
+
+        public static DBAccessController GetInstance(bool reset = false)
+        {
+            return instance ??= new DBAccessController(reset);
+        }
+
+
+
+
+        private void Reset()
+        {
+
+        }
+    }
+}
