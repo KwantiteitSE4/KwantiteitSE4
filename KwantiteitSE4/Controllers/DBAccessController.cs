b2b050736a9ccf7a80c2fad8a3e21a02a32eedaa
Alexander <alexander.de.haan@student.nhlstenden.com>
2022-09-27 11:32:35 +0200

    Different SQLite Package Created SqliteConnection field

--- KwantiteitSE4/Controllers/DBAccessController.cs
+++ KwantiteitSE4/Controllers/DBAccessController.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.IO;
-using System.Data;
+using Microsoft.Data.Sqlite;
 
 namespace KwantiteitSE4.Controllers
 {
     public class DBAccessController
     {
         private static DBAccessController instance;
+        private const string DBFILENAME = "KwanDB.db";
+        private SqliteConnection connection;
 
         private DBAccessController(bool reset = false) 
         {
             if (reset) Reset();
+            string connectionString = new SqliteConnectionStringBuilder()
+            {
+                DataSource = DBFILENAME,
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                ForeignKeys = true
+            }.ToString();
+            connection = new SqliteConnection(connectionString);
         }
 
         public static DBAccessController GetInstance(bool reset = false)
