30edd01f4b1ace0e57658c51d1be50a1deb77134
TheSillyP <Petjenorden@gmail.com>
2022-10-05 14:46:08 +0200

    Merged two front end branches

--- KwantiteitSE4/ClientApp/src/components/CreateGame.js
+++ KwantiteitSE4/ClientApp/src/components/CreateGame.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect } from 'react';
-import { fetchAllGames } from '../redux/actions/getGames';
+import { fetchAllPlayers } from '../redux/actions/getPlayers';
 import { useSelector, useDispatch } from 'react-redux';
 import 'antd/dist/antd.css';
 import './CreateGame.css';
@@ -22,13 +22,13 @@ for (let i = 1; i < maxLegCount; i++) {
 
 export const CreateGame = () => {
   useEffect(() => {
-    dispatch(fetchAllGames());
+    dispatch(fetchAllPlayers());
   }, [])
 
-  const games = useSelector((state) => state.games.value);
+  const players = useSelector((state) => state.players.value);
   const dispatch = useDispatch();
 
-  console.log(games);
+  console.log(players);
   return (
       <Space>
         <Row gutter={[16, 16, 16, 16]}>
@@ -36,10 +36,10 @@ export const CreateGame = () => {
             <div className='player1'>
               <Card title="Speler 1">
                 <Image className='player-portrait' src="https://dartfreakz.nl/wp-content/uploads/2021/07/WLDMTCHPLAY-RD1-VANGERWEN37A-768x511.jpg" /><br></br>
-                <Select defaultValue="Wie is speler 1">
-                {games.map((item, index) => (
-                  <Option value={item.player1.name} key={index}>
-                    {item.player1.name}
+                <Select defaultValue="Wie is speler 1 van de wedstrijd">
+                {players.map((item, index) => (
+                  <Option value={item.name} key={index}>
+                    {item.name}
                   </Option>
                 ))}
                 </Select>
@@ -47,12 +47,12 @@ export const CreateGame = () => {
             </div>
           </Col>
           <Col className='gutter-row' span={9}>
-            <div>
-              <Card title="Datum & tijd" size="small">
-                <DatePicker />
-                <DatePicker picker='time' />
+            <div className='middleArea'>
+              <Card className='date-and-time' title="Datum & tijd" size="small">
+                <DatePicker className='date-picker' />
+                <DatePicker className='time-picker' picker='time' />
               </Card>
-              <Card className='sets-and-legs'>
+              <Card className='sets-and-legs'title="Sets & Legs" size="small">
                 Best of sets:
                 <Select>
                   {sets}
@@ -62,13 +62,24 @@ export const CreateGame = () => {
                   {legs}
                 </Select>
               </Card>
+              <Card className="button-card">
+                <Link to='MatchScreen'>
+                  <Button>Submit form</Button>
+                </Link>
+            </Card>
             </div>
           </Col>
           <Col Col className='gutter-row' span={8}>
             <div className='player2'>
               <Card title="Speler 2">
                 <Image className='player-portrait' src="https://e2.365dm.com/21/10/768x432/skysports-rob-cross-darts-world-grand-prix_5535273.jpg?20211117161322" /><br></br>
-                <Input className='player-name-input' placeholder="Voer naam in van speler 2" />
+                <Select defaultValue="Wie is speler 2 van de wedstrijd">
+                {players.map((item, index) => (
+                  <Option value={item.name} key={index}>
+                    {item.name}
+                  </Option>
+                ))}
+                </Select>
               </Card>
             </div>
           </Col>
--- KwantiteitSE4/ClientApp/src/components/MatchOverview.js
+++ KwantiteitSE4/ClientApp/src/components/MatchOverview.js
@@ -1,99 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Avatar, Divider, List, Skeleton } from 'antd';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import 'antd/dist/antd.css';
 import './MatchOverview.css';
-import { useSelector, useDispatch, connect } from 'react-redux';
-import { fetchAllGames, fetchGames } from '../redux/actions/getGames';
-import { GET_GAMES } from '../redux/types';
 
 export const MatchOverview = () => {
   const displayName = MatchOverview.name;
 
-  useEffect(() => {
-    dispatch(fetchAllGames());
-  }, [])
-
-  const games = useSelector((state) => state.games.value);
-  const dispatch = useDispatch();
-
-  console.log(games);
-
-  //   const [loading, setLoading] = useState(false);
-  //   const [data, setData] = useState([]);
-
-  //   const loadMoreData = () => {
-  //     if (loading) {
-  //       return;
-  //     }
-
-  //     setLoading(true);
-  //     fetch('https://localhost:5001/Games')
-  //       .then((res) => res.json())
-  //       .then((body) => {
-  //         setData([...data, ...body]);
-  //         setLoading(false);
-  //       })
-  //       .catch(() => {
-  //         setLoading(false);
-  //       });
-  //   };
-
-  //   useEffect(() => {
-  //     loadMoreData();
-  //   }, []);
-
   return (
-    <div>
-      <h2>{games[0]?.player1.name}</h2>
-    </div>
-    // <div>
-    //     <div className='matchscrolllist'
-    //         id="scrollableDiv"
-    //         style={{
-    //             height: 400,
-    //             overflow: 'auto',
-    //             padding: '0 16px',
-    //             border: '1px solid rgba(140, 140, 140, 0.35)',
-    //         }}
-    //     >
-    //         <InfiniteScroll
-    //             dataLength={data.length}
-    //             next={loadMoreData}
-    //             hasMore={data.length < 50}
-    //             loader={
-    //             <Skeleton
-    //                 avatar
-    //                 paragraph={{
-    //                 rows: 1,
-    //                 }}
-    //                 active
-    //             />
-    //             }
-    //             endMessage={<Divider plain>All Matches Loaded</Divider>}
-    //             scrollableTarget="scrollableDiv"
-    //         >
-    //             <List
-    //                 header={<div><h3>Matches</h3> </div>}
-    //                 dataSource={data}
-    //                 renderItem={(item) => (
-    //                     <List.Item key={item.gameID}>
-    //                     <List.Item.Meta
-    //                         avatar={<Avatar src={item.picture} />}
-    //                         title={ (new Date(item.gameDateTime)).toLocaleString() + ': ' + item.player1.name + ' vs ' + item.player2.name}
-    //                         description={'Number of Sets: ' + item.numberOfSets + '  |  Number of Legs: ' + item.numberOfLegs}
-    //                     />
-    //                     <Link className='matchoverview__data__edit' to='/MatchEditor'>
-    //                         <img className='matchoverview__data__edit__icon' src="https://cdn.iconscout.com/icon/free/png-256/edit-1780339-1517827.png"/>
-    //                     </Link>
-    //                     <img className='matchoverview__data__status__icon' src="https://cdn.iconscout.com/icon/premium/png-256-thumb/done-2606464-2184156.png"/>
-    //                     </List.Item>
-    //                 )}
-    //             />
-    //         </InfiniteScroll>
-    //     </div>
-  /* <div className='matchoverview'>
+        <div className='matchoverview'>
             <table className='matchoverview__data'>
                     <tr>
                         <th className='matchoverview__data__title' colSpan='5'>Matches</th>
@@ -118,8 +31,7 @@ export const MatchOverview = () => {
                             <img className='matchoverview__data__status__icon' src="https://cdn.iconscout.com/icon/premium/png-256-thumb/done-2606464-2184156.png"/>
                         </td>
                     </tr>
-            </table>
-        </div> */
-  // </div>
+                </table>
+        </div>
   )
 }
