5701b04f75dd6d453b922ea428cbecf2a6f1e405
monodrail <denick15@gmail.com>
2022-10-12 14:22:38 +0200

    first

--- KwantiteitSE4/ClientApp/src/App.js
+++ KwantiteitSE4/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Router, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Main } from './components/Main';
 import { PlayerOverview } from './components/PlayerOverview';
@@ -12,13 +12,17 @@ import { MatchScreen } from './components/MatchScreen';
 const App = () => {
   return (
       <Layout>
-        <Route exact path='/' component={Main} />
-        <Route path='/PlayerOverview' component={PlayerOverview} />
-        <Route path='/PlayerEditor' component={PlayerEditor} />
-        <Route path='/MatchOverview' component={MatchOverview} />
-        <Route path='/MatchEditor' component={MatchEditor} />
-        <Route path='/CreateGame' component={CreateGame} />
-        <Route path='/MatchScreen' component={MatchScreen} />
+          <Router>
+            <Routes>
+              <Route exact path='/' component={Main} />
+              <Route path='/PlayerOverview' component={PlayerOverview} />
+              <Route path='/PlayerEditor' component={PlayerEditor} />
+              <Route path='/MatchOverview' component={MatchOverview} />
+              <Route path='/MatchEditor' component={MatchEditor} />
+              <Route path='/CreateGame' component={CreateGame} />
+              <Route path='/MatchScreen' component={MatchScreen} />
+            </Routes>
+          </Router>
       </Layout>
   )
 };
--- KwantiteitSE4/ClientApp/src/index.js
+++ KwantiteitSE4/ClientApp/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,14 +9,14 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+//const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  rootElement);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
+root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+);
 registerServiceWorker();
