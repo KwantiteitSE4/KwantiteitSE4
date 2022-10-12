636fd63a838201b6c033a560336391e68c4c7de5
Alexander <alexander.de.haan@student.nhlstenden.com>
2022-10-12 11:27:55 +0200

    Can now post an array of three throws at once

--- KwantiteitSE4/Controllers/ThrowsController.cs
+++ KwantiteitSE4/Controllers/ThrowsController.cs
@@ -54,6 +54,19 @@ namespace KwantiteitSE4.Controllers
             return -1;
         }
 
+        [HttpPost("EnterThrows")]
+        public void EnterThrows([Bind("turnID,multiplier,throwScore", "turnID,multiplier,throwScore", "turnID,multiplier,throwScore")] IList<Throw> throws)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (Throw t in throws)
+                {
+                    _context.Add(t);
+                }
+                _context.SaveChanges();
+            }
+        }
+
         // POST: Throws/Edit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
