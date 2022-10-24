cfdb5f54cc33a20a4bf0d8744aa6b2d80761379b
Nick de Boer <denick15@gmail.com>
2022-10-24 14:12:44 +0200

    lint wat nu weer

--- KwantiteitSE4/ClientApp/src/components/CreateGame.test.js
+++ KwantiteitSE4/ClientApp/src/components/CreateGame.test.js
@@ -1,7 +1,7 @@
 ﻿import postNewGame from './CreateGame'
 describe('Create game and return game id', () => {
   it('should create a game and return the game id', async () => {
-    let game = await postNewGame();
+    const game = await postNewGame();
     expect(game).not.toBeNull();
     expect(game.id).not.toBeNull();
   }, 10000);
