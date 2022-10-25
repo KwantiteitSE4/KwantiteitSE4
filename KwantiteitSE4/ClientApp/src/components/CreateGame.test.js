16f8e436a0341cdbd143dc4614826f08bd871c0d
musaab <musaab.azawi@student.nhlstenden.com>
2022-10-25 09:22:54 +0200

    PostNewGame function exported. Test case added

--- KwantiteitSE4/ClientApp/src/components/CreateGame.test.js
+++ KwantiteitSE4/ClientApp/src/components/CreateGame.test.js
@@ -1,8 +1,27 @@
-﻿import { postNewGame } from './CreateGame'
+﻿import { postNewGame } from './CreateGame';
 describe('Create game and return game id', () => {
   it('should create a game and return the game id', async () => {
-    const game = await postNewGame();
+    const testObject = {
+      player1ID: 1,
+      player1: {
+        playerID: 1,
+        name: 'Nick de Boer'
+      },
+      player2ID: 2,
+      player2: {
+        playerID: 2,
+        name: 'Patrick Norden'
+      },
+      winnerID: null,
+      winner: null,
+      numberOfSets: 3,
+      numberOfLegs: 3,
+      gameDateTime: '2022-10-18T14:25:02.0447046',
+      startPlayerID: 1
+    };
+    const game = postNewGame(testObject);
     expect(game).not.toBeNull();
     expect(game.id).not.toBeNull();
+    expect(console.log(JSON.game));
   }, 10000);
 });
